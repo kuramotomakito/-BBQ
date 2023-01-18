2afbc6800ae7f2b8e69279765ae41f0085c970cc
minagi <m.fujishima0615@gmail.com>
2023-01-19 08:22:10 +0900

    goul

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import './header.scss'
 import FirstView from '../../asset/photo/first view.jpg'
 import phonephoto from '../../asset/photo/phone.svg'
+import firstviewsp from '../../asset/photo/firstviewsp.jpg'
 
 
 const returnTop = () => {
@@ -13,7 +14,11 @@ const returnTop = () => {
 function index() {
   return (
     <header>
-      <h1><img src={FirstView} alt='江ノ島マリーナBBQサイト' /></h1>
+      <h1>
+        <div className='firstview'><img src={FirstView} alt='江ノ島マリーナBBQサイト' /></div>
+        <div className='firstviewsp'><img src={firstviewsp} alt="江ノ島マリーナBBQサイト" /></div>
+      </h1>
+
       <button className='yoyaku' onClick={returnTop}><a href='#'>予約する</a></button>
       <button className='phone' onClick={returnTop}><a href='#'><img src={phonephoto} alt='phone' /></a></button>
     </header>
