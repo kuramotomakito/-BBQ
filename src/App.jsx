c7cfdeef0879405943b12bc4f5ccd0e71b10ac75
minagi <m.fujishima0615@gmail.com>
2023-01-18 16:15:32 +0900

    0118

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import Attention from './components/Attention'
 import Access from './components/Access'
 import Content from './components/Content'
 import Footer from './components/Footer'
+
 function App() {
   return (
     <>
-    <HeaderCom />
-    <article>
-    <main>
-    <Service />
-    <Situetion />
-    <Plan />
-    <Attention />
-    <Access />
-    <Content />
-</main>
-</article>
-<Footer />
+      <HeaderCom />
+      <article>
+        <main>
+          <Service />
+          <Situetion />
+          <Plan />
+          <Attention />
+          <Access />
+          <Content />
+        </main>
+      </article>
+      <Footer />
     </>
   )
 }
