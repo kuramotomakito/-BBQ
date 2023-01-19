742826d4c175e2e0dacd31328e25eb264c030d23
minagi <m.fujishima0615@gmail.com>
2023-01-19 09:49:53 +0900

    0119

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 // import './header.scss'
-import FirstView from '../../asset/photo/first view.jpg'
+import FirstView from '../../asset/photo/cruise 1.png'
 import phonephoto from '../../asset/photo/phone.svg'
-import firstviewsp from '../../asset/photo/firstviewsp.jpg'
+import firstviewsp from '../../asset/photo/cruisesp.png'
+import logo from '../../asset/photo/logo.svg'
 
 
 const returnTop = () => {
@@ -17,6 +18,8 @@ function index() {
       <h1>
         <div className='firstview'><img src={FirstView} alt='江ノ島マリーナBBQサイト' /></div>
         <div className='firstviewsp'><img src={firstviewsp} alt="江ノ島マリーナBBQサイト" /></div>
+        <div className='logo'><img src={logo} alt='江ノ島マリーナBBQサイト' /></div>
+        <div className='catchcopy'><p><b className='enosima'>江ノ島</b>でちょっとかわった<b className='BBQ'>BBQ</b>を</p></div>
       </h1>
 
       <button className='yoyaku' onClick={returnTop}><a href='#'>予約する</a></button>
